8ed336daafc97f97051bdf01463b41f4ae2d2354
John Travers <johnk.travers@gmail.com>
2019-11-04 07:07:10 -0700

    Add grade_in_course instance method

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
 
   has_many :student_courses
   has_many :courses, through: :student_courses
+
+  def grade_in_course(course)
+    student_courses.find_by(course_id: course.id).grade
+  end
 end
