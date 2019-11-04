cfb12e7db58d4c208d5bce3ab3adab71ac6c6478
John Travers <johnk.travers@gmail.com>
2019-11-04 07:36:03 -0700

    Add test to retrieve a course's students in order of descending grade

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Course, type: :model do
     it { should have_many :student_courses }
     it { should have_many(:students).through(:student_courses) }
   end
+
+  describe 'instance methods' do
+    it 'students by grade' do
+      calc = Course.create!(name: 'Calculus III')
+
+      duke = Student.create!(name: 'Duke Silver')
+      leslie = Student.create!(name: 'Leslie Knope')
+      april = Student.create!(name: 'April Ludgate')
+
+      calc.student_courses.create!(grade: 87.4, student_id: duke.id)
+      calc.student_courses.create!(grade: 93.2, student_id: leslie.id)
+      calc.student_courses.create!(grade: 78.3, student_id: april.id)
+
+      expect(calc.students_by_grade).to eq([leslie, duke, april])
+    end
+  end
 end
