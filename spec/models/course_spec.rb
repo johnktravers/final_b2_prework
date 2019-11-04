79fd205206707e7c56cd596901f99830b65f3c5e
John Travers <johnk.travers@gmail.com>
2019-11-04 06:38:19 -0700

    Add tests to establish many to many relationship between students and courses

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Course, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should have_many :student_courses }
+    it { should have_many(:students).through(:student_courses) }
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Student, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should have_many :student_courses }
+    it { should have_many(:courses).through(:student_courses) }
+  end
 end
