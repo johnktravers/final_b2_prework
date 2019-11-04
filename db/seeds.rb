3330c46ab859bcb3aaf7e7a83a6d44bd3a29c1ba
John Travers <johnk.travers@gmail.com>
2019-11-04 09:58:53 -0700

    Add seed data for students and courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+calc = Course.create(name: 'Calculus III')
+german = Course.create(name: 'German Linguistics')
+history = Course.create(name: 'History of Ancient Civilizations')
+
+duke = Student.create(name: 'Duke Silver')
+leslie = Student.create(name: 'Leslie Knope')
+april = Student.create(name: 'April Ludgate')
+
+duke.student_courses.create(grade: 85.3, course_id: calc.id)
+duke.student_courses.create(grade: 91.0, course_id: history.id)
+
+leslie.student_courses.create(grade: 98.6, course_id: calc.id)
+leslie.student_courses.create(grade: 95.3, course_id: german.id)
+leslie.student_courses.create(grade: 100.0, course_id: history.id)
+
+april.student_courses.create(grade: 73.4, course_id: german.id)
+april.student_courses.create(grade: 82.3, course_id: history.id)
