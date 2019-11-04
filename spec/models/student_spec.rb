0a760e0d42e678fa330636f0ef425df922d1f1e2
John Travers <johnk.travers@gmail.com>
2019-11-04 07:06:47 -0700

    Add test for instance method to retrieve a student's grade for a specific course

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Student, type: :model do
     it { should have_many :student_courses }
     it { should have_many(:courses).through(:student_courses) }
   end
+
+  describe 'instance methods' do
+    it 'grade in course' do
+      greg = Student.create!(name: 'Greg Pikitis')
+      calc = Course.create!(name: 'Calculus III')
+      greg.student_courses.create!(grade: 87.4, course_id: calc.id)
+
+      expect(greg.grade_in_course(calc)).to eq(87.4)
+    end
+  end
 end
